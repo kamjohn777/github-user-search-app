406572fba923a29228538719747593e14fd941b7
Kameron Johnson <kamjohn79.kdj@gmail.com>
2024-04-27 15:13:22 -0500

    updating the js logic

--- github-user-search-app/github-user-search-app/starter-code/script.js
+++ github-user-search-app/github-user-search-app/starter-code/script.js
@@ -1,7 +1,3 @@
-
-
-
-
 $(document).ready(function() {
   $('.light-theme-button').click(function() {
     $('.body-wrap').removeClass('dark-theme');
@@ -20,15 +16,7 @@ $(document).ready(function() {
     $('.ui.segment.container').addClass('dark-theme');
     console.log('Dark theme activated');
   });
-
-
-
-  
 });
-
-
-
-
 
 $(document).ready(function() {
   $('.ui.button').click(function() {
@@ -41,33 +29,43 @@ $(document).ready(function() {
       .then(response => response.json())
       .then(data => {
         // Set the text of the #username span to the user's login
-        $('#username').text(data.login);
+        $('#username').text(data.login || "No information");
 
         // Set the src of the #profile-picture img to the user's avatar
-        $('#profile-picture').attr('src', data.avatar_url);
+        $('#profile-picture').attr('src', data.avatar_url || "");
 
         // Set the number of repos
-        $('#num-repos').parent().text(`${data.public_repos}`);
+        $('#num-repos').parent().text(data.public_repos || "0");
 
         // Set the number of followers
-        $('#num-followers').parent().text(`${data.followers}`);
+        $('#num-followers').parent().text(data.followers || "0");
 
         // Set the number of people the user is following
-        $('#num-following').parent().text(`${data.following}`);
+        $('#num-following').parent().text(data.following || "0");
 
         // Set the date the user joined
-        $('#joined').text(new Date(data.created_at).toLocaleDateString());
+        $('#joined').text(data.created_at ? new Date(data.created_at).toLocaleDateString() : "No information");
 
         // Set the user's location
-        $('#location').text(data.location);
+        $('#location').text(data.location || "No information");
 
         // Set the user's Twitter link
-        $('#twitter-link').attr('href', `https://twitter.com/${data.twitter_username}`);
-        $('#twitter-link').text(data.twitter_username);
+        if (data.twitter_username) {
+          $('#twitter-link').attr('href', `https://twitter.com/${data.twitter_username}`);
+          $('#twitter-link').text(data.twitter_username);
+        } else {
+          $('#twitter-link').attr('href', "#");
+          $('#twitter-link').text("No information");
+        }
 
         // Set the user's GitHub link
-        $('#github-link').attr('href', data.html_url);
-        $('#github-link').text(data.login);
+        if (data.html_url) {
+          $('#github-link').attr('href', data.html_url);
+          $('#github-link').text(data.login);
+        } else {
+          $('#github-link').attr('href', "#");
+          $('#github-link').text("No information");
+        }
       })
       .catch(error => {
         // Handle the error here
